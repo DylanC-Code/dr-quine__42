a0a27412918d55460986421e15b60005fc89474b
DylanC-Code <dylanc.code@gmail.com>
2025-11-02 22:57:09 +0100

    feat(Collen C): Add .gitignore and implement main source file

---
+++ C/Colleen/srcs/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Outside comment */
+void	show_source(void)
+{
+	char *data = "#include <stdio.h>\n\n/* Outside comment */\nvoid\tshow_source(void)\n{\n\tchar *data = %c%s%c;\n\tprintf(data, 34, data, 34);\n}\n\nint\tmain(void)\n{\n\t/* Inside comment */\n\tshow_source();\n\treturn (0);\n}\n";
+	printf(data, 34, data, 34);
+}
+
+int	main(void)
+{
+	/* Inside comment */
+	show_source();
+	return (0);
+}
