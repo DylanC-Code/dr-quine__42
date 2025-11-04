c4a00c7e7d2c9b8ecce5f847238d3ae708a8f4e3
DylanC-Code <dylanc.code@gmail.com>
2025-11-04 17:22:07 +0100

    style(C): Improve formatting of comments in Colleen.c for better readability

--- C/srcs/Colleen.c
+++ C/srcs/Colleen.c
@@ -3,16 +3,20 @@
 #define DQUOTE 34
 #define NEWLINE 10
 
-/* Outside comment */
+/* 
+	Outside comment
+ */
 void	show_source(void)
 {
-	char *data = "#include <stdio.h>%1$c%1$c#define DQUOTE 34%1$c#define NEWLINE 10%1$c%1$c/* Outside comment */%1$cvoid	show_source(void)%1$c{%1$c	char *data = %2$c%3$s%2$c;%1$c	printf(data, NEWLINE, DQUOTE, data);%1$c}%1$c%1$cint	main(void)%1$c{%1$c	/* Inside comment */%1$c	show_source();%1$c	return (0);%1$c}%1$c";
+	char *data = "#include <stdio.h>%1$c%1$c#define DQUOTE 34%1$c#define NEWLINE 10%1$c%1$c/* %1$c	Outside comment%1$c */%1$cvoid	show_source(void)%1$c{%1$c	char *data = %2$c%3$s%2$c;%1$c	printf(data, NEWLINE, DQUOTE, data);%1$c}%1$c%1$cint	main(void)%1$c{%1$c	/* %1$c		Inside comment%1$c	 */%1$c	show_source();%1$c	return (0);%1$c}%1$c";
 	printf(data, NEWLINE, DQUOTE, data);
 }
 
 int	main(void)
 {
-	/* Inside comment */
+	/* 
+		Inside comment
+	 */
 	show_source();
 	return (0);
 }
